d123d2d33b940a5ea57fa359fa92b8ed85551e5a
Renan Machado <Mirandarenan078@gmail.com>
2025-05-09 21:31:25 -0300

    Mensagem do commit

--- Controllers/UsuarioController.php
+++ Controllers/UsuarioController.php
@@ -24,7 +24,7 @@ class UsuarioController extends Controller {
             $senha = $_POST['senha'];
 
 
-            $user->setCadastro($nome, $faculdade, $cargo, $matricula, $senha);
+            $user->setCadastroAluno($nome, $faculdade, $cargo, $matricula, $senha);
 
             $_SESSION['msg'] = ['type' => 'success', 'message' => 'Usuário cadastrado com sucesso!'];
 
@@ -33,64 +33,71 @@ class UsuarioController extends Controller {
 
     }
 
-    public function verificarLogin() {
+    public function LoginAluno() {
+       $usuario = new usuario();
+
+       $matriculaDigitada =$_POST['Matricula'];
+       $senha = $_POST['senha'];
+
+       $result = $usuario->verificarLoginAluno($matriculaDigitada, $senha);
+        if ($result) {
+            echo "Entrada permitida";
+        } else {
+            echo "Entrada não permitida";
+
+        }
+    }
+
+    public function LoginProfessor() {
 
         $usuario = new usuario();
 
-        $sql = $this->db->prepare("SELECT matricula, senha from usuario");
-        $sql->execute();
+        $emailDigitado = $_POST['emailProfessor'];
+        $senha = $_POST['senhaProfessor'];
 
-        $matriculaDigitada = $_POST['matricula'];
-        $senhaDigitada = $_POST['senha'];
-            if ($sql->rowCount () > 0) {
-                $usuario = $sql->fetch(PDO::FETCH_ASSOC);
+        $result = $usuario->verificarLoginProfessor( $emailDigitado, $senha);
+        if ($result) {
+            echo "Entrada permitida";
+        } else {
+            echo "Entrada não permitida";
 
-                if ($matriculaDigitada == $usuario['matricula'] and password_verify($senhaDigitada, $usuario['senha'])) {
-                    echo 'Entrada permitida';
-                }
-                else{
-                    echo 'Entrada não permitida';
-                }
-
-            }else {
-                echo 'usuario não encontrado';
         }
+
     }
 
-    public function trocarSenha() {
-        $matricula = $_POST['matriculaSetSenha'];
+    public function mudarSenhaAluno() {
+
+        $usuario = new usuario();
+
+        $matricula = $_POST['matriculaTrocarSenha'];
         $senhaAntiga = $_POST['senhaAntiga'];
         $novaSenha = $_POST['novaSenha'];
 
-        $sql = $this->db->prepare('SELECT senha FROM usuario WHERE matricula = :matricula');
-        $sql->bindValue(':matricula', $matricula);
-        $sql->execute();
-    
-        if ($sql->rowCount() > 0) {
-            $usuario = $sql->fetch(PDO::FETCH_ASSOC);
-    
-            if (password_verify($senhaAntiga, $usuario['senha'])) {
+        $result = $usuario->trocarSenhaAluno($matricula, $senhaAntiga, $novaSenha);
+        if ($result) {
+            echo "Senha alterada com sucesso";
+
+        }else {
+            echo "Senha naão pode ser alterada";
             
-                $senhaCriptografada = password_hash($novaSenha, PASSWORD_DEFAULT);
-    
-                $update = $this->db->prepare("UPDATE usuario SET senha = :novaSenha WHERE matricula = :matricula");
-                $update->bindValue(':novaSenha', $senhaCriptografada);
-                $update->bindValue(':matricula', $matricula);
-                $update->execute();
-    
-                echo "Senha alterada com sucesso!";
-            } else {
-                echo "Senha antiga incorreta.";
-            }
-        } else {
-            echo "Usuário não encontrado.";
         }
     }
-    
 
-    
+    public function mudarSenhaProfessor() {
 
+        $usuario = new usuario();
 
-}
-    
-		
+        $emailProfessor = $_POST['emailProfessor'];
+        $senhaAntiga = $_POST['senhaAntiga'];
+        $novaSenha = $_POST['novaSenha'];
+
+        $result = $usuario->trocarSenhaProfessor($emailProfessor, $senhaAntiga, $novaSenha);
+        if ($result) {
+            echo "Senha alterada com sucesso";
+
+        }else {
+            echo "Senha naão pode ser alterada";
+            
+        }
+    }
+} 
